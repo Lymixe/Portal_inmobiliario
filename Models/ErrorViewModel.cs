a2fcd3ed6eafc2fe99cd66a62fc510be07b29eeb
Lymixe <peraltacenepoemily@gmail.com>
2025-09-26 20:22:21 -0500

    main

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Portal_inmobiliario.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
